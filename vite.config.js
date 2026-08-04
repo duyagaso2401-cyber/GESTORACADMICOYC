a97d9e7afd1232a206f9b47886479072088207f3
Adan Jimenez <duyagaso2401@gmail.com>
2026-08-04 17:34:46 -0500

    Ajustar root y emptyOutDir en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'gestor-academico/dist',
+  //root: 'gestor-academico/dist',
   server: {
     port: 5000,
     host: true
@@ -14,5 +14,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist'
+    emptyOutDir: true
   }
 });
